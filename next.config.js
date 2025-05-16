ec5f0cc7e997c078209afec24a69b6a2ceb7b720
🍀 <50286230+XERA-2011@users.noreply.github.com>
2025-05-16 14:16:07 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,5 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === 'production';
-const repoName = 'xera-2011.github.io';
 module.exports = {
   output: 'export',                  // 启用静态导出
-  basePath: isProd ? `/${repoName}` : '',
-  assetPrefix: isProd ? `/${repoName}/` : '',
   images: { unoptimized: true },     // 关闭 Next.js 默认图片优化
 };

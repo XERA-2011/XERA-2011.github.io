ada25db737e7b042537dfad0272a0a3e04c9ca93
🍀 <50286230+XERA-2011@users.noreply.github.com>
2025-05-16 14:11:45 +0800

    deploy test

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const isProd = process.env.NODE_ENV === 'production';
-const repoName = 'XERA-2011.github.io';
+const repoName = 'xera-2011.github.io';
 module.exports = {
   output: 'export',                  // 启用静态导出
   basePath: isProd ? `/${repoName}` : '',
